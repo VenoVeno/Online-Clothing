ff893fe4d39ccc891fc658c287e51624a6bec3c8
VenoVeno <venodpi@gmail.com>
2020-04-15 22:39:15 +0530

    Test

--- src/firebase/test.js
+++ src/firebase/test.js
@@ -6,3 +6,10 @@ const firestore = firebase.firestore();
 firestore.collection('users').doc('yG5qE2hV7AzBTrZztMP9').collection('cart-item').doc('ItSnfDNKvBziUUs0iTZY');
 firestore.doc('/users/yG5qE2hV7AzBTrZztMP9/cart-item/ItSnfDNKvBziUUs0iTZY');
 firestore.collection('/users/yG5qE2hV7AzBTrZztMP9/cart-item');
+
+{Redux_Installation}
+//npm install redux redux-logger react-redux
+        {or}
+// npm install --save react-redux
+// npm install --save redux
+// npm i --save redux-logger
