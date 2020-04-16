2fec94b9ab657c95cefb5a71b082e13af1d7d309
VenoVeno <venodpi@gmail.com>
2020-04-16 18:37:04 +0530

    Cart Reducer added with ADD_ITEM without implementation

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -4,9 +4,11 @@ import './custom-button.styles.scss';
 
 //Receive this as children from the Sign In Component and print inside as {children}
 
-const CustomButton = ({ children, isGoogleSign, ...otherButtonProps }) => (
+const CustomButton = ({ children, isGoogleSign, inverted, ...otherButtonProps }) => (
     <button
-        className={`${isGoogleSign ? 'google-sign-in' : ''} custom-button`}
+        className={
+            `${inverted ? 'inverted' : ''}${isGoogleSign ? 'google-sign-in' : ''} custom-button`
+        }
         {...otherButtonProps}>
         {children}
     </button>
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,4 +1,5 @@
 //default import as const Cart....... export default Cart.....
 export const CartActionTypes = {
-    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+    ADD_ITEMS: 'ADD_ITEMS'
 }
