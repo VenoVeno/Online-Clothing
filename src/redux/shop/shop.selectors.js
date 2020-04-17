ef46c0b81d12278c88ca4b559197e578a6bc640a
VenoVeno <venodpi@gmail.com>
2020-04-17 20:49:12 +0530

    Added old versions in selector as a comment line and made small change in Collection Component

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -2,18 +2,27 @@ import { createSelector } from 'reselect';
 
 const selectShop = (state) => state.shop;
 
+//SIMPLE SHOP COLLECTION SELECTION BEFORE CONVERTING ARRAY INTO OBJECTS
 export const selectCollections = createSelector(
     [selectShop],
     (shop) => shop.collections
 )
 
+//UPDATED COLLECTION SELECTION PREVIEW AFTER CONVERTING ARRAY TO OBJECT-- SHOPDATA.
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
     (collections) => Object.keys(collections).map(key => collections[key])
 )
 
+//DATA NORMALIZATION TECHNIQUE CONVERTING ID MATCH TO OBJECT MATCH
 export const selectCollection = (collectionUrlParam) =>
     createSelector(
         [selectCollections],
         (collections) => collections[collectionUrlParam]
     )
+
+//const COLLECTION_MAP_ID = {hats: 1,sneakers: 2,jackets: 3,womens: 4,mens: 5}
+//ID MATCH USING COLLECTION_MAP_ID WITH COLLECTION ID
+//      (collections) => collections.find(
+//             collection => collection.id === COLLECTION_MAP_ID[collectionUrlParam]
+//  )
