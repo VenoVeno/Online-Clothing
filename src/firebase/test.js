ae2980d31346bfdaafd85b309eee218e92d4bd80
VenoVeno <venodpi@gmail.com>
2020-04-18 09:16:13 +0530

    Before adding Styled Components Into App (Working) with HEROKU_DEPLOY

--- src/firebase/test.js
+++ src/firebase/test.js
@@ -7,12 +7,32 @@ firestore.collection('users').doc('yG5qE2hV7AzBTrZztMP9').collection('cart-item'
 firestore.doc('/users/yG5qE2hV7AzBTrZztMP9/cart-item/ItSnfDNKvBziUUs0iTZY');
 firestore.collection('/users/yG5qE2hV7AzBTrZztMP9/cart-item');
 
-{Redux_Installation}
+{ Redux_Installation }
 //npm install redux redux-logger react-redux
-        {or}
+{ or }
 // npm install --save react-redux
 // npm install --save redux
 // npm i --save redux-logger
 
-{Redux_Persist}
-///npm install redux-persist
+{ Redux_Persist }
+// npm install redux-persist
+
+
+{ STYLED_COMPONENTS }
+// npm install styled-components
+
+EXAMPLE
+import styled from 'styled-components';
+
+// CSS_IN_JS--USING CSS INSIDE JS
+const Text = styled.div`
+    color:red;
+    font-size: 20px;
+    border: ${ ({ isActive })} => 
+        isActive ? '1px solid black' : '3px dotted red'
+`;
+
+<Text isActive={true} >I am a Component</Text>
+
+CAN_ALSO_IMPORT_THIS_COMPONENT_IN_ANOTHER_COMPONENT;
+AND_USE_IN_ANOTHER_COMPONENT_WITH_PASSING_PROPS_TO_THE_COMPONENT;
